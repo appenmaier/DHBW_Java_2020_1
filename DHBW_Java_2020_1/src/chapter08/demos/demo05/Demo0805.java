5e0afaed7e9e39f7ab9dfbcc530f96d0a1a30c9d
appenmaier <daniel.appenmaier@gmail.com>
2021-05-18 12:07:53 +0200

    ...

---
+++ DHBW_Java_2020_1/src/chapter08/demos/demo05/Demo0805.java
@@ -0,0 +1,39 @@
+package chapter08.demos.demo05;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Byteorientiertes Lesen und Schreiben von Objekten (Serialisierung)
+ * 
+ * @author appenmaier
+ *
+ */
+public class Demo0805 extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public void init() {
+		Model model = Model.getInstance();
+		model.initLogFile();
+	}
+	
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Demo 8.5");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public void stop() {
+		Model model = Model.getInstance();
+		model.saveLog();
+	}
+
+}
